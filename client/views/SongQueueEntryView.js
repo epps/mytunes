ae118a07e2480ee9292791c9de4cd0f0ffa07a48
MKS-students <mks.students@hackreactor.com>
2015-07-16 10:30:35 -0700

    Added initial space for songQueue

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -1,4 +1,20 @@
 // SongQueueEntryView.js - Defines a backbone view class for the song queue entries.
 var SongQueueEntryView = Backbone.View.extend({
-  // your code here!
+  // set the tagname
+  tagName: 'tr',
+
+  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
+
+  // events: {
+  //   'click': function() {
+  //     // this.model.play();
+  //     // call an enqueue method on the song's model
+  //     this.model.addSongToQueue();
+  //   }
+  // },
+
+  render: function(){
+    return this.$el.html(this.template(this.model.attributes));
+  }
+
 });
